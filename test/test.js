2590c6c2924093246a157e797971779ecda8b2e4
paolostyle <tuptus95@gmail.com>
2018-03-14 02:07:57 +0100

    simple tests, configured CI

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 const flatten = require('../index.js');
+const Vinyl = require('vinyl');
 
-test('flatten json', () => {
-    let inputFile = fs.readFileSync('test/input/to-flat.json');
-    flatten.write()
+let inputFile, outputFile, mockedFile;
 
+beforeEach(() => {
+    inputFile = fs.readFileSync('test/jsons/input.json');
+    outputFile = fs.readFileSync('test/jsons/output.json');
+    mockedFile = new Vinyl({
+        contents: inputFile
+    });
+
+    flatten().write(mockedFile);
+});
+
+test('mock should be buffer', () => {
+    expect(mockedFile.isBuffer()).toBeTruthy();
+});
+
+test('should properly flatten the json file', function() {
+    expect(mockedFile.contents).toEqual(outputFile);
 });
